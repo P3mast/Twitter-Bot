444a990cf22053aaed4750dbd40a9cbc857bb36a
P3mast <87942178+P3mast@users.noreply.github.com>
2021-11-09 17:47:21 +0100

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,8 +1,8 @@
 #Twitter API_keys and token keys
 from termcolor import colored
-api_key = "xZQdW3inJYpyTvh8dAIZY8xtd"
-api_secret_key = "aBj9r2bPeL4diTkdGzMIA25iVImaJGZiHR4qD6jM6V0LuuHrEh"
-api_access_token = "1420025450575933446-YUxEKszqZcKucnWem5udXUZNkC95bE"
-api_access_token_secret = "04ntLJUgP56QImg3H2BRZ96fOcYcwtxifYDCDYordJ1rs"
-api_bearer_token = 'AAAAAAAAAAAAAAAAAAAAAMRgSAEAAAAAT2JP7OuwVnKCobF%2B9rRmfgRz8Xw%3DALoZt4RykOD2nty5TtPfE3T4WMSZXdBnW4ZwUOBNnGf9bKuBJh'
+api_key = ":)"
+api_secret_key = ":)"
+api_access_token = ";)"
+api_access_token_secret = "0.o"
+api_bearer_token = ";)"
 print(colored("---Api keys imported---", "green"))
